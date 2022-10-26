6af43a4a4477123a00c979629ede0781abb1e40a
Mike Oram <Mikeoram01@gmail.com>
2022-10-26 14:13:31 +0100

    updated the header text

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className={"weatherAppHeader"}>
             <h1 className={"weatherAppTitle"}>SlothSan's Weather App</h1>
-            <p className={"weatherAppText"}>Please see below for a day forecast for your area.</p>
+            <p className={"weatherAppText"}>Below is the current weather for your area.</p>
             <p className={"weatherAppText"}>More features to come!</p>
         </header>
     )
---
+++ src/Components/WeatherDisplayContainer/FiveDayForecastDisplayCards/FiveDayForecastDisplayCards.js
@@ -0,0 +1,9 @@
+const FiveDayForecastDisplayCards = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FiveDayForecastDisplayCards
