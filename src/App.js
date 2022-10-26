55fad512fc35c4ee62344f1ff39851244c1e0808
Mike Oram <Mikeoram01@gmail.com>
2022-10-26 14:09:46 +0100

    Created the footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from "./Components/Header/Header";
 import WeatherDisplayContainer from "./Components/WeatherDisplayContainer/WeatherDisplayContainer";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <WeatherDisplayContainer/>
+      <Footer />
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,12 @@
 const Footer = () => {
-
+    return (
+    <footer>
+        <a target="_blank" href="https://github.com/SlothSan">
+            Created by Mike Oram 2022
+            <i className="fa-brands fa-github fa-spin-hover"></i>
+        </a>
+    </footer>
+    )
 }
 
 export default Footer
