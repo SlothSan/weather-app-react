0684d76c806469ab568cc6e47ad909267f657c87
Mike Oram <Mikeoram01@gmail.com>
2022-10-28 10:12:41 +0100

    created buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import WeatherDisplayContainer from "./Components/WeatherDisplayContainer/WeatherDisplayContainer";
 import Footer from "./Components/Footer/Footer";
+import OptionsSection from "./Components/OptionsSection/OptionsSection";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <OptionsSection />
       <WeatherDisplayContainer/>
       <Footer />
     </div>
---
+++ src/Components/OptionsSection/OptionsSection.js
@@ -0,0 +1,10 @@
+const OptionsSection = () => {
+    return (
+        <section className={"optionsSection"}>
+            <button>Current Weather</button>
+            <button>Five Day Forecast</button>
+        </section>
+    )
+}
+
+export default OptionsSection
